17472a4179c80fab891b3e8083584ed19a835b30
TheShadow29 <ark.sadhu2904@gmail.com>
2015-09-26 20:49:15 +0530

    mostly working

--- m10.m
+++ m10.m
@@ -4,20 +4,28 @@ vidDevice = imaq.VideoDevice('winvideo', 1, 'YUY2_640x480', ...
 
 hVideoIn = vision.VideoPlayer;
 hVideoIn.Name = 'Original Video';
-hVideoIn.Position = [30 100 640 480];
+hVideoIn.Position = [30 30 640 480];
 
 hVideoOut = vision.VideoPlayer;
 hVideoOut.Name = 'Fingers Tracking Video';
 hVideoOut.Position = [700 100 640 480];
 
+%vectors
+vect = zeros(2,10);
+len_vec = zeros(1,10);
+max_len_vec = len_vec;
+
+mt = zeros(1,5);
+%frames
+frm=0;
 while 1
     img = step(vidDevice);
-    [m n]=size(img);
+    [m, n]=size(img);
     J = rgb2gray(img);
     background = imopen(J,strel('disk',15));
     i2 = J + background;
     i3 = imadjust(i2);
-    level = 70/255;
+    level = 100/255;
     %level = graythresh(i3);
     max_area = 70000;
     min_area=60000;
@@ -43,7 +51,7 @@ while 1
     cent = [graindata.Centroid];
     out = wb;
     stats=regionprops(out,'Centroid');
-    if length(stats)
+    if ~isempty(stats)
 
     cx=cent(1);
     cy=cent(2);
@@ -88,22 +96,18 @@ while 1
     c1 = bwconncomp(final);
     c1.NumObjects;
     g1 = regionprops(c1);
-    are = [g1.Area];
-    are_sorted = sort(are);
-    mt = zeros(1,5);
-    if length(are_sorted)==5
-        mt = ones(1,5);
-    end
-    if isempty(are_sorted)
-        mt=zeros(1,5);
-    end
+  
     
+        
     pts = zeros(2,6);
     pts(1:2,1)=[cx,cy];
-    [m n]=size(final);
+    pts2 = zeros(2,6);
+    pts(1:2,1)=[cx,cy];
+    [m ,n]=size(final);
     ind = 2;
     flag =0;
-    imshow(final);
+    final2 = final;
+    imshow(final2);
     for idx=1:c1.NumObjects
         gr = false(size(wb));
         gr(c1.PixelIdxList{idx})= true;
@@ -123,16 +127,41 @@ while 1
             end
         end
     end
+    
+    
+    
     viscircles([cx cy],10);
     for ii=2:c1.NumObjects+1
         line([cx,pts(2,ii)],[cy,pts(1,ii)]);
     end
     
-    
-else
-    step(hVideoOut,out);
+    %if c1.NumObjects==5
+        for ii=1:5
+            vect(1:2,ii)=[cx - pts(2,ii+1), cy - pts(1,ii+1)];
+            len_vec(ii) = sqrt(sum(vect(:,ii).^2));
+        end
+    %end
+    if c1.NumObjects==5
+        for ii=1:c1.NumObjects
+            if max_len_vec(ii)<len_vec(ii)
+                max_len_vec(ii)=len_vec(ii);
+            end
+            if len_vec(ii)<.66*max_len_vec(ii)
+                mt(ii)=0;
+            else
+                mt(ii)=1;
+            end
+            
+        end
     end
-                
+
+        
+else
+    %mt=zeros(1,5);
+    %step(hVideoOut,out);
+    end
+    %step(hVideoOut,wb);
+    mt      
 end
 release(hVideoOut);
 release(hVideoIn);
